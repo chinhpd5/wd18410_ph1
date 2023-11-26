1d5f345336b1a895cf60529f1e4cd34290df8787
chinhpd5 <chinhpd5@fpt.edu.vn>
2023-11-26 18:03:06 +0700

    add

--- quanlyoto/add.php
+++ quanlyoto/add.php
@@ -10,9 +10,69 @@
     $errXuatXu ='';
     $errImage ='';
 
+    $isCheck= true;
+    
+
+    if(isset($_POST["submit"])){
+        $tenLoaiXe= trim($_POST["tenLoaiXe"]);
+        $xuatXu= trim($_POST["xuatXu"]);
+        $idDanhMuc = $_POST["idDanhMuc"];
+        $mauSac= $_POST["mauSac"];
+        $image = $_FILES["hinhAnh"];
+        
+        echo "<pre>";
+        print_r([$tenLoaiXe, $xuatXu,$idDanhMuc,$mauSac,$image]);
+
+        if($tenLoaiXe == ''){
+            $isCheck =false;
+            $errTenLoaiXe ="Cần nhập tên xe";
+        }
+
+        if($xuatXu == ''){
+            $isCheck =false;
+            $errXuatXu ="Cần nhập tên xe";
+        }
+
+        $filename = $image["name"];
+        if($filename){
+            $extension = pathinfo($filename,PATHINFO_EXTENSION);
+            // echo $extension;
+            $arrAllow =['png','jpg','jpeg'];
+            //phủ định !
+            if(!in_array($extension,$arrAllow)){
+                $isCheck= false;
+                $errImage ="Cần nhập file ảnh";
+            }
+
+        }else{
+            $isCheck =false;
+            $errImage ="Cần nhập 1 file";
+        }
+
+        if($isCheck){
+            $filename = time().$filename;
+            $dir = "uploads/$filename";
+            if(move_uploaded_file($image["tmp_name"],$dir)){
+                $sql = "INSERT INTO xe(tenLoaiXe,xuatXu,idDanhMuc,mauSac,hinhAnh)
+                    VALUES ('$tenLoaiXe','$xuatXu','$idDanhMuc','$mauSac','$filename')";
+
+                $result = $conn->query($sql);
+                if($result){
+                    header('Location: index.php');
+                }else{
+                    echo "thêm thất bại";
+                }
+            }
+        }
+
+
+    }
+
+    
+
 ?>
 
-<form action="add.php" method="post">
+<form action="add.php" method="post" enctype="multipart/form-data">
     <label for="">Tên xe</label>
     <input type="text" name="tenLoaiXe" value="<?= $tenLoaiXe?>">
     <span style="color:red"><?= $errTenLoaiXe?></span> <br>
@@ -45,8 +105,31 @@
     </select> <br>
 
     <label for="">Hình ảnh</label>
-    <input type="file" name="hinhAnh">
+    <input type="file" name="hinhAnh" id="inputImage">
     <span style="color:red"><?= $errImage?></span> <br>
+    <img src="" alt="" id="imgPreview" style="width:150px"> <br>
 
     <input type="submit" name ="submit" value ="Gửi">
 </form>
+
+<script>
+    const inputText = document.getElementById('inputImage');
+    const img = document.getElementById('imgPreview');
+    inputText.addEventListener('change',function(event){
+        const file = event.target.files[0];
+        // console.log(file);
+        const filename = file.name;
+
+        const arr = filename.split('.');
+        // console.log(arr);
+        const extension = arr[arr.length - 1];
+        // console.log(extension);
+
+        const arrAllow =['png','jpg','jpeg'];
+
+        if(arrAllow.includes(extension)){
+            img.src = URL.createObjectURL(file);
+        }
+
+    })
+</script>
